e4cfd932caabd30d43b649f1278ccc13abd2284e
nakayama-ryu1 <tosu.nakayama@gmail.com>
2025-09-05 00:02:07 +0900

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [aspectRatio()],
+}
